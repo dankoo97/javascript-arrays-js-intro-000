ee43b1906d0ff7c292bd9e0dd35abb6873c8696b
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 00:06:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
